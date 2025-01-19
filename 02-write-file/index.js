d58c38f688e3074542574eca732c59d718d71b52
Makar Siarko <cerko2002@gmail.com>
2025-01-19 13:51:32 +0300

    feat: task completed 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const filePath = path.join(__dirname, 'output.txt');
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Enter text (type "exit" to quit or press Ctrl+C):');
+
+rl.on('line', (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    console.log('Goodbye!');
+    rl.close();
+  } else {
+    writeStream.write(input + '\n');
+  }
+});
+
+rl.on('close', () => {
+  console.log('Process terminated.');
+  writeStream.end();
+});
+
+process.on('SIGINT', () => {
+  console.log('\nGoodbye!');
+  rl.close();
+});
